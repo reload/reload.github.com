f09584557347d20c9f5d8716eb2da79b7539bacf
matis-dk <mathiasp50@gmail.com>
2022-02-16 17:13:34 +0100

    Showing multiple karma kings for a given week

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,41 +1,28 @@
-async function loadData(url, headerid, contentid) {
-    let json = null;
+async function renderKing (json, headerid, contentid) {
+    if(!json) return;
+
     const contentElm = document.querySelector(contentid);
 	const headerElm = document.querySelector(headerid);
 
     let strContent = '';
     let strHeader = '';
 
-    if(json = await getData(url)) {
+    strHeader += '<h1 id="project_title">' + json.title + ': ' + json.full_name + ' 😃</h1>';
+    strHeader += '<h2 id="project_tagline">- ugens helt i uge '+ json.week +', '+ json.year +'</h2>';
+    headerElm.innerHTML = strHeader;
 
-        strHeader += '<h1 id="project_title">' + json.title + ': ' + json.full_name + ' 😃</h1>';
-        strHeader += '<h2 id="project_tagline">- ugens helt i uge '+ json.week +', '+ json.year +'</h2>';
-        headerElm.innerHTML = strHeader;
-
-        strContent += '<div id="avatar"><img src="'+ json.avatar_url + '" /></div>';
-        strContent += '<div id="votes">';
-        strContent += '<h2>Hvad kollegerne siger 👏</h2>';
+    strContent += '<div id="avatar"><img src="'+ json.avatar_url + '" /></div>';
+    strContent += '<div id="votes">';
+    strContent += '<h2>Hvad kollegerne siger 👏</h2>';
 
 
-		Object.entries(json.votes).forEach((entry) => {
-		  const [key, value] = entry;
-		  strContent += '<p class="votes">' + value + '</p>';
-		});
+    Object.entries(json.votes).forEach((entry) => {
+        const [key, value] = entry;
+        strContent += '<p class="votes">' + value + '</p>';
+    });
 
-		strContent += '</div>';
+    strContent += '</div>';
 
-        contentElm.innerHTML = strContent;                
-    }
+    contentElm.innerHTML = strContent;                
 }
 
-async function getData(url) {
-
-    try {
-        response = await (await fetch(url)).json();
-    } catch(e) {
-        console.log('error');
-    }
-    console.log(response);
-
-    return await response;
-}
